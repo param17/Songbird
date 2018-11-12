ea85822b15870725f77c016b029c9e291e2e935b
nikhil sulegaon <nikhilsulegaon@gmail.com>
2018-11-11 19:24:47 -0700

    Adding script to test model.

--- synthesizer.py
+++ synthesizer.py
@@ -1,20 +1,21 @@
 import io
+
 import numpy as np
 import tensorflow as tf
+from scipy.misc import imread, imresize
+
 from hparams import hparams
 from models import create_model
-from text import text_to_sequence
 from util import audio
 
 
 class Synthesizer:
-    def load(self, checkpoint_path, model_name='tacotron'):
+    def load(self, checkpoint_path, vgg19_path, model_name='tacotron'):
         print('Constructing model: %s' % model_name)
-        inputs = tf.placeholder(tf.int32, [1, None], 'inputs')
-        input_lengths = tf.placeholder(tf.int32, [1], 'input_lengths')
+        inputs = tf.placeholder(tf.int32, [1, hparams.image_dim, hparams.image_dim, 3], 'inputs')
         with tf.variable_scope('model') as _:
             self.model = create_model(model_name, hparams)
-            self.model.initialize(inputs, input_lengths)
+            self.model.initialize(inputs, vgg19_path)
             self.wav_output = audio.inv_spectrogram_tensorflow(self.model.linear_outputs[0])
 
         print('Loading checkpoint: %s' % checkpoint_path)
@@ -23,12 +24,12 @@ class Synthesizer:
         saver = tf.train.Saver()
         saver.restore(self.session, checkpoint_path)
 
-    def synthesize(self, text):
-        cleaner_names = [x.strip() for x in hparams.cleaners.split(',')]
-        seq = text_to_sequence(text, cleaner_names)
+    def synthesize(self, image_path):
+        raw_image = imread(image_path, mode='RGB')
+        processed_image = imresize(raw_image, (224, 224, 3))
+
         feed_dict = {
-            self.model.inputs: [np.asarray(seq, dtype=np.int32)],
-            self.model.input_lengths: np.asarray([len(seq)], dtype=np.int32)
+            self.model.inputs: [np.asarray(processed_image, dtype=np.float32)],
         }
         wav = self.session.run(self.wav_output, feed_dict=feed_dict)
         wav = audio.inv_preemphasis(wav)
---
+++ test.py
@@ -0,0 +1,37 @@
+import argparse
+import datetime
+import os
+
+from synthesizer import Synthesizer
+
+
+def __get_output_wav_path(args):
+    output_dir = os.path.join(args.base_dir, 'out')
+    os.makedirs(output_dir, exist_ok=True)
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
+    return os.path.join(output_dir, 'eval-{}.wav'.format(timestamp))
+
+
+def __run_eval(args):
+    synth = Synthesizer()
+    synth.load(args.checkpoint, os.path.join(args.base_dir, args.vgg19_path))
+    wav_path = __get_output_wav_path(args)
+    print('Synthesizing: %s' % wav_path)
+    with open(wav_path, 'wb') as f:
+        f.write(synth.synthesize(args.image_path))
+    print('Wav - {} generated successfully!'.format(wav_path))
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--checkpoint', required=True, help='Path to model checkpoint')
+    parser.add_argument('--image_path', required=True, help='Path to the test image')
+    parser.add_argument('--base_dir', default=os.path.expanduser('~/tacotron'))
+    parser.add_argument('--vgg19_path', default='training/vgg19/vgg19.npy')
+    args = parser.parse_args()
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+    __run_eval(args)
+
+
+if __name__ == '__main__':
+    main()
